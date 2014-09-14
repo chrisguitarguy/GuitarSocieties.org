bab471e6520bfe222e8992cfaf0a9ebe19b5b885
Christopher Davis <cdavis9999@gmail.com>
2014-09-14 15:25:35 -0400

    Give the admin site some custom branding

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
+from django.utils.translation import ugettext_lazy as _
+
+admin.site.site_header = _('Guitar Societies')
+admin.site.site_title = _('Guitar Societies')
+admin.site.index_title = _('Guitar Society Admin')
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
