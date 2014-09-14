2b8bcf1092a0fedef414ef16a59ec81cbc7150fb
Christopher Davis <cdavis9999@gmail.com>
2014-09-13 21:12:30 -0400

    Show societies on the home page

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='homepage'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^societies/', include('societies.urls', namespace='societies')),
+    url(r'^', include('societies.urls', namespace='societies')),
 )
